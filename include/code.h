632d37f13b4232e0bfd6ab017297ccd71c344c76
Scythe504 <hellfireprince98@gmail.com>
2025-11-17 13:13:15 +0530

    code.h

---
+++ include/code.h
@@ -0,0 +1,23 @@
+#ifndef code_h
+#include <unordered_map>
+#include <string>
+
+// dest bits [3..5]
+std::unordered_map<std::string, std::string> destMap;
+// jump bits [0..2]
+std::unordered_map<std::string, std::string> jumpMap;
+// bits a|c1|c2|c3|c4|c5|c6
+std::unordered_map<std::string, std::string> compMap;
+// comp bits C instruction[6..12]
+std::string TranslateComp(std::string comp);
+
+// jump bits C instruction [3..5]
+std::string TranslateJump(std::string jump);
+
+// jump bits C instruciton [0..2]
+std::string TranslateDest(std::string dest);
+std::string ToBinary15Bit(int addr);
+
+std::string TranslateCInstruction(std::string dest, std::string comp, std::string jump);
+
+#endif
